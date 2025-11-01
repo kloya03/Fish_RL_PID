36e775eed4d806edabc9bb8ae23d17d1b799d456
Kartik Dell_AIO <kloya@clemson.edu>
2025-11-01 13:16:08 -0400

    camera calibrate scripts

---
+++ Camera_Calibrate_runcam/Calibrate_extrinsic_parameters.py
@@ -0,0 +1,169 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 13 16:50:47 2024
+
+@author: kloya
+"""
+import cv2
+import numpy as np
+import time
+import pickle
+
+def nothing(x):
+    pass
+
+cap = cv2.VideoCapture(0)
+width = 1920
+height = 1080
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+cap.set(cv2.CAP_PROP_FPS, 60)  # Set fps to 1
+fps = cap.get(cv2.CAP_PROP_FPS)
+frame_count = 0
+
+cv2.namedWindow('Trackbars')
+cv2.moveWindow('Trackbars', 1400, 100)
+cv2.resizeWindow('Trackbars',350, 250)  # Set the desired size (width, height)
+
+cv2.createTrackbar('h_Low', 'Trackbars', 3, 255, nothing)
+cv2.createTrackbar('h_Hi', 'Trackbars', 45, 255, nothing)
+
+cv2.createTrackbar('satLow', 'Trackbars', 140, 255, nothing)
+cv2.createTrackbar('satHigh', 'Trackbars', 255, 255, nothing)
+
+cv2.createTrackbar('valLow', 'Trackbars', 150, 255, nothing)
+cv2.createTrackbar('valHigh', 'Trackbars', 245, 255, nothing)
+
+# Load the camera fisheye calibration data
+file_path = "/home/kloya/catkin_fishcam_ws/src/fish_pc/scripts/Camera_Calibrate_runcam/fisheye_calibration_1080p_25.pkl"
+with open(file_path, "rb") as file:  # "rb" means read in binary mode
+    data = pickle.load(file)
+camera_matrix, distortion_coeffs = data
+
+# Print the variables to verify
+print("Camera Matrix:\n", camera_matrix)
+print("Distortion Coefficients:\n", distortion_coeffs)
+
+# new_camera_matrix, roi = cv2.getOptimalNewCameraMatrix(camera_matrix, dist_coeffs, (width, height), 1, (width, height))
+new_camera_matrix = cv2.fisheye.estimateNewCameraMatrixForUndistortRectify\
+    (camera_matrix, distortion_coeffs, (width, height), np.eye(3), balance=1)
+map1, map2 = cv2.fisheye.initUndistortRectifyMap(camera_matrix, distortion_coeffs,\
+            np.eye(3), new_camera_matrix, (width, height), cv2.CV_16SC2)
+  
+# Assume the real-world size of the object (in meters)
+object_size = np.array([0.2, 0.1])   #  in m (w, h)
+# Define positions
+# positions = ['left', 'center', 'right']
+calibration_data = {}
+
+# for position in positions:
+#     print(f"Calibrating for {position} position...")
+sum_pixels_per_m = 0
+sum_pixel_size = np.array([0,0])
+pixels_per_m_f = 100 # final pixels/m
+err = 1
+frame_initialize = 1050
+frame_count = 0
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        break
+    frame_count += 1
+    # frame = cv2.flip(frame, 0)
+    # frame  = cv2.flip(frame, 1)
+    # cv2.imshow("frame", frame)
+    # Undistort the frame
+    # undistorted_frame = cv2.undistort(frame, camera_matrix, dist_coeffs, None, new_camera_matrix)
+    undistorted_frame = cv2.remap(frame, map1, map2, interpolation=cv2.INTER_LINEAR)
+    # Convert to HSV
+    hsv = cv2.cvtColor(undistorted_frame, cv2.COLOR_BGR2HSV)
+    # Make the specified region black based on the ROI
+    # hsv[:110, :] = 0  # Top boundary
+    # hsv[-110:, :] = 0  # Bottom boundary
+    # hsv[:, :110] = 0  # Left boundary
+    # hsv[:, -100:] = 0  # Right boundary
+
+    #   Create mask for orange color
+    hl = cv2.getTrackbarPos('h_Low', 'Trackbars')
+    hu = cv2.getTrackbarPos('h_Hi', 'Trackbars')
+    sl = cv2.getTrackbarPos('satLow', 'Trackbars')
+    su = cv2.getTrackbarPos('satHigh', 'Trackbars')
+    vl = cv2.getTrackbarPos('valLow', 'Trackbars')
+    vu = cv2.getTrackbarPos('valHigh', 'Trackbars')
+
+    lower_orange = (hl,sl,vl)   # Adjust these values based on the orange color in your environment
+    upper_orange = (hu,su,vu)
+    mask1 = cv2.inRange(hsv, lower_orange, upper_orange)
+    mask1= cv2.morphologyEx(mask1, cv2.MORPH_CLOSE, np.ones((5, 5), np.uint8))
+    mask1= cv2.GaussianBlur(mask1, (5, 5), 0)
+    # cv2.imshow("mask", mask2)
+    # cv2.imshow("Calibrate2", mask1)
+    # Find contours
+    contours, _ = cv2.findContours(mask1, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # print()
+    
+    if not contours:
+        # cv2.imshow("mask", mask1)
+        print("No contours found. Exiting.")
+        continue
+    else:
+        # Calculate contour areas and sort them
+        contour_areas = [(cv2.contourArea(c), c) for c in contours]
+        contour_areas.sort(key=lambda x: x[0], reverse=True)
+        
+        # Select the top 1 contours by area
+        largest_contours = [c for area, c in contour_areas[:1]]
+        rect = cv2.minAreaRect(largest_contours[0])
+        box = cv2.boxPoints(rect)
+        bounding_box = np.intp(box)
+        # print(bounding_box)
+        
+        if frame_count > frame_initialize:
+            frame_no = frame_count - frame_initialize
+            # Calculate the number of pixels that span the object
+            pixel_size = np.array(sorted(rect[1],reverse=True))
+            pixels_per_m = (pixel_size/object_size)
+
+        
+            # Calculate the cumulative average number of pixels that span the object
+            sum_pixel_size = sum_pixel_size + pixel_size
+            Cum_pixel_size = sum_pixel_size/frame_no  # width in pixels
+            sum_pixels_per_m = pixels_per_m + sum_pixels_per_m
+            Cum_pixels_per_m = sum_pixels_per_m/frame_no
+            
+            if err < 1e-4:
+                err_mm = (Cum_pixel_size/pixels_per_m_f - object_size)*1000
+                print("Converged pixels per meter = ",pixels_per_m_f," with an error of ",err_mm,"mm")
+                break
+            else:
+                pixels_per_m_prev = pixels_per_m_f
+                pixels_per_m_f = sum(Cum_pixels_per_m*(object_size/sum(object_size)))
+                err = abs(pixels_per_m_f - pixels_per_m_prev)
+                print(frame_count,'frame',Cum_pixels_per_m,pixels_per_m,Cum_pixel_size,pixel_size)
+                continue
+        else:
+            print('~',frame_count,'frame')
+
+    cv2.drawContours(undistorted_frame, [bounding_box], -1, (0, 0, 255), 2)
+#     # Display the frame
+    cv2.imshow("Calibrate", undistorted_frame)
+    # time.sleep()
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    
+calibration_data = {'pixels_per_m': pixels_per_m_f,
+'camera_matrix': camera_matrix,
+'distortion_coeffs': distortion_coeffs}
+# Wait for user input before proceeding to the next position
+# input(f"Calibration for {position} position complete. Press Enter to continue to the next position...")
+
+
+np.savez('runcam_calibration_data_1080p.npz', **calibration_data)
+    
+print("Camera calibration successful.")
+
+# Release everything if job is finished
+cap.release()
+cv2.destroyAllWindows()
+    
---
+++ Camera_Calibrate_runcam/fisheye_calibration.py
@@ -0,0 +1,106 @@
+import numpy as np
+import cv2 as cv
+import glob
+import pickle
+import os
+
+#################### FIND CHESSBOARD CORNERS ##############################
+
+chessboardSize = (10,7)
+# frameSize = (1280, 720)
+frameSize = (1920, 1080)
+# Termination criteria
+criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.1)
+
+# Prepare object points
+objp = np.zeros((1, chessboardSize[0] * chessboardSize[1], 3), np.float32)
+objp[0, :, :2] = np.mgrid[0:chessboardSize[0], 0:chessboardSize[1]].T.reshape(-1, 2)
+
+size_of_chessboard_squares_mm = 25  # Adjust this to the real size of your squares
+objp = objp * size_of_chessboard_squares_mm
+
+# Arrays to store points
+objpoints = []  # 3D points
+imgpoints = []  # 2D points
+
+# Load images
+images = glob.glob(os.path.expanduser('~/catkin_fishcam_ws/src/fish_pc/scripts/Camera_Calibrate_runcam/runcam_images_1080p/*.png'))
+if not images:
+    print("No images found. Please check the directory path.")
+else:
+    for image_path in images:
+        img = cv.imread(image_path)
+        print(f"Processing: {image_path}")
+        if img is None:
+            print(f"Unable to read image: {image_path}")
+            continue
+        gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+        # Find chessboard corners
+        ret, corners = cv.findChessboardCorners(gray, chessboardSize, None)
+
+        if ret:
+            objpoints.append(objp)
+            corners2 = cv.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
+            imgpoints.append(corners2)
+
+            # Draw and display corners
+            cv.drawChessboardCorners(img, chessboardSize, corners2, ret)
+            cv.imshow('img', img)
+            cv.waitKey(500)
+
+    cv.destroyAllWindows()
+
+############## FISHEYE CALIBRATION ########################################
+
+K = np.zeros((3, 3))
+D = np.zeros((4, 1))
+rvecs = []
+tvecs = []
+
+# Fisheye calibration
+ret, K, D, rvecs, tvecs = cv.fisheye.calibrate(
+    objpoints,
+    imgpoints,
+    frameSize,
+    K,
+    D,
+    rvecs,
+    tvecs,
+    flags=cv.fisheye.CALIB_RECOMPUTE_EXTRINSIC + cv.fisheye.CALIB_FIX_SKEW
+)
+
+# Save calibration data
+with open("fisheye_calibration_1080p_25.pkl", "wb") as f:
+    pickle.dump((K, D), f)
+
+################ UNDISTORTION #############################################
+
+img = cv.imread('/home/kloya/catkin_fishcam_ws/src/fish_pc/scripts/Camera_Calibrate_runcam/runcam_images_1080p/img55.png')
+h, w = img.shape[:2]
+new_K = cv.fisheye.estimateNewCameraMatrixForUndistortRectify(K, D, (w, h), np.eye(3), balance=1)
+
+map1, map2 = cv.fisheye.initUndistortRectifyMap(K, D, np.eye(3), new_K, (w, h), cv.CV_16SC2)
+undistorted_img = cv.remap(img, map1, map2, interpolation=cv.INTER_LINEAR)
+
+# Save undistorted image
+cv.imwrite("fisheye_undistorted_1080p.png", undistorted_img)
+
+# Display images
+# cv.imshow("Original", img)
+# cv.imshow("Undistorted", undistorted_img)
+# cv.waitKey(0)
+# cv.destroyAllWindows()
+
+################ REPROJECTION ERROR #######################################
+
+mean_error = 0
+for i in range(len(objpoints)):
+    imgpoints2, _ = cv.fisheye.projectPoints(objpoints[i], rvecs[i], tvecs[i], K, D)
+    # Reshape to ensure both arrays have the same shape
+    imgpoints2 = np.squeeze(imgpoints2)  # Shape becomes (70, 2)
+    imgpoints[i] = np.squeeze(imgpoints[i])  # Shape becomes (70, 2)
+    error = cv.norm(imgpoints[i], imgpoints2, cv.NORM_L2) / len(imgpoints2)
+    mean_error += error
+
+print(f"Total reprojection error: {mean_error / len(objpoints)}")
